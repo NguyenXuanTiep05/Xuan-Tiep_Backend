c82b42dc4febb2ae5eeca6a835125b382bcfc07d
Nguyen Xuan Tiep <nguyenpetr2005@gmail.com>
2026-07-29 18:48:58 +0200

    fix structure

--- Src/Models/Finances/CreateFinanceExpenseDto.cs
+++ Src/Models/Finances/CreateFinanceExpenseDto.cs
@@ -4,7 +4,5 @@ namespace Auth.Src.Models.Finances;
 public record CreateFinanceExpenseDto
 {
 	public decimal Value { get; set; }
-	public string Currency { get; set; } = "CZK";
 	public string Description { get; set; } = "None";
-	public int UserId { get; set; }
 }
